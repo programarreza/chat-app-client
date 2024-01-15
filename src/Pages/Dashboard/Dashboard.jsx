16da8435776ec5bdf8f1213ace46ecb6e2f8c7eb
programarreza <programarreza@gmail.com>
2024-01-15 14:26:20 +0600

    send message to database

--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -11,10 +11,11 @@ const Dashboard = () => {
   const { user } = useAuth();
   const { loggedInUser } = useLoggedInUser();
   const [conversations, setConversations] = useState([]);
-  const [messages, setMessages] = useState({});
+  const [messages, setMessages] = useState([]);
+  const [message, setMessage] = useState("");
 
   console.log(loggedInUser?._id);
-  const image = "https://i.postimg.cc/W31nybCs/image3.png";
+  const image = "https://i.postimg.cc/mkphFd7b/IMG-20200811-141534.jpg";
 
   useEffect(() => {
     const fetchConversations = async () => {
@@ -25,16 +26,34 @@ const Dashboard = () => {
   }, [loggedInUser?._id]);
 
   console.log(conversations);
-  console.log(messages);
+  console.log(messages?.receiver?.receiverId);
 
-  const fetchMessage = async (conversationId, user) => {
+  const fetchMessages = async (conversationId, user) => {
     const res = await axiosLocal.get(`/api/message/${conversationId}`);
-    setMessages({messages: res, receiver: user});
-    console.log(res?.data);
+    const resData = res?.data;
+    setMessages({ messages: resData, receiver: user, conversationId });
+    console.log("fetchMessages", resData);
   };
+
+  const userSendMessage = {
+    conversationId: messages?.conversationId,
+    senderId: loggedInUser?._id,
+    message,
+    receiverId: messages?.receiver?.receiverId,
+  };
+  
+  const sendMessage = async () => {
+    await axiosLocal.post("/api/message", userSendMessage).then((res) => {
+      if (res.status === 200) {
+        console.log(res);
+        setMessage("");
+      }
+    });
+  };
+
   return (
     <div className="w-full flex justify-center bg-gray-100">
-      {/* users area */}
+      { /* users area */}
       <div className="w-[25%] h-screen bg-[#cbe6f9]">
         <div className="m-2">
           {/* logged in user profile */}
@@ -64,7 +83,7 @@ const Dashboard = () => {
                   <>
                     <div
                       className="cursor-pointer flex "
-                      onClick={() => fetchMessage(conversationId, user)}
+                      onClick={() => fetchMessages(conversationId, user)}
                     >
                       <div className="flex  items-center p-4 ">
                         <div className="avatar mr-4">
@@ -82,7 +101,7 @@ const Dashboard = () => {
                 );
               })
             ) : (
-              <div className="text-center text-lg font-semibold mt-24">
+              <div className="text-center text-lg font-semibold my-12">
                 No Conversation
               </div>
             )}
@@ -95,29 +114,31 @@ const Dashboard = () => {
       {/* chatting area */}
       <div className="w-[75%] border-2 h-screen ">
         {/* chatting heading */}
-        <div className="flex justify-between items-center shadow-md bg-[#cbe6f9]">
-          <div className="flex  items-center  p-4 ">
-            <div className="avatar mr-4">
-              <div className="w-10 border border-black rounded-full cursor-pointer">
-                <img src={image} alt="profile image" />
+        {messages?.receiver?.fullName && (
+          <div className="flex justify-between items-center shadow-md bg-[#cbe6f9]">
+            <div className="flex  items-center  p-4 ">
+              <div className="avatar mr-4">
+                <div className="w-10 border border-black rounded-full cursor-pointer">
+                  <img src={image} alt="profile image" />
+                </div>
+              </div>
+              <div>
+                <h2>{messages?.receiver?.fullName}</h2>
+                <p className="text-sm">{messages?.receiver?.email}</p>
               </div>
             </div>
-            <div>
-              <h2>Shafikul islam</h2>
-              <p className="text-sm">Active</p>
+            <div className="flex gap-8  mr-4 ">
+              <FaPhone size={22} className="cursor-pointer" />
+              <IoVideocam size={28} className="cursor-pointer" />
             </div>
           </div>
-          <div className="flex gap-8  mr-4 ">
-            <FaPhone size={22} className="cursor-pointer" />
-            <IoVideocam size={28} className="cursor-pointer" />
-          </div>
-        </div>
+        )}
 
         {/* message body */}
         <div className="h-[75%] border w-full overflow-scroll border-b">
           <div className=" px-4 py-6">
             {messages?.messages?.length > 0 ? (
-              messages.messages.map(({ message, user: { id } = {} }) => {
+              messages?.messages?.map(({ message, user: { id } = {} }) => {
                 return (
                   <div
                     key={id}
@@ -140,23 +161,30 @@ const Dashboard = () => {
         </div>
 
         {/* message input area */}
-        <div className="mx-2 mt-6 flex items-center justify-center">
-          <LuBadgePlus
-            size={30}
-            className="text-blue-500 mr-4 cursor-pointer"
-          />
+        {messages?.receiver?.fullName && (
+          <div className="mx-2 mt-6 flex items-center justify-center">
+            <LuBadgePlus
+              size={30}
+              className="text-blue-500 mr-4 cursor-pointer"
+            />
 
-          <input
-            type="text"
-            placeholder="Type Message"
-            className="w-[60%] p-2 rounded-md outline-none shadow-sm relative"
-          />
+            <input
+              type="text"
+              placeholder="Type Message"
+              className="w-[60%] p-2 rounded-md outline-none shadow-sm relative"
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+            />
 
-          <BsSendCheck
-            size={30}
-            className="-ml-12 z-10 cursor-pointer text-blue-500"
-          />
-        </div>
+            <BsSendCheck
+              onClick={() => sendMessage()}
+              size={30}
+              className={`-ml-12 z-10 cursor-pointer text-blue-500 ${
+                !message && "pointer-events-none"
+              }`}
+            />
+          </div>
+        )}
       </div>
     </div>
   );
